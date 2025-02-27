a4fc004b9e9e5fb71b9832c155173ea87f22ac6e
Elkantar <samsonthibault76@gmail.com>
2025-02-27 04:46:02 +0100

    update js menu link

--- src/js/header.js
+++ src/js/header.js
@@ -1,5 +1,26 @@
-fetch("../../html/header.html")
+/*fetch("../../html/header.html")
 .then((response) => response.text())
 .then((data) => {
     document.body.insertAdjacentHTML("afterbegin", data);
 });
+*/
+// Détecte le niveau du fichier actuel
+const getBasePath = () => {
+    const depth = window.location.pathname.split("/").length - 2; 
+    return "../".repeat(depth);
+};
+
+const basePath = getBasePath();
+
+fetch(basePath + "html/header.html")
+    .then((response) => response.text())
+    .then((data) => {
+        document.body.insertAdjacentHTML("afterbegin", data);
+
+        // Corriger tous les liens après l'insertion du header
+        document.querySelectorAll("a").forEach((link) => {
+            if (!link.href.startsWith("http")) { 
+                link.href = basePath + link.getAttribute("href");
+            }
+        });
+    });
